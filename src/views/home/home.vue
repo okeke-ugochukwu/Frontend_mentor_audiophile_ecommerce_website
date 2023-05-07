77e070228c2571366a6ac124350f0ac5e36687f2
Ugo <okekeugochukwu.dev@gmail.com>
2023-05-07 14:36:52 +0100

    About section done. { Responsive }

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -63,7 +63,7 @@
 
    <!-- FEATURED PRODUCTS -->
    <section
-      class="featured-products py-[60px] md:py-[43px] lg:pt-[84px] lg:pb-[100px]"
+      class="featured-products py-[60px] md:py-[48px] lg:pt-[84px] lg:pb-[100px]"
    >
       <!-- MARGIN -->
       <div class="margin flex flex-col items-center gap-6 md:gap-8 lg:gap-12">
@@ -86,7 +86,7 @@
 
                <picture class="relative z-2">
                   <source media="(min-width: 1024px)" srcset="@/assets/imgs/home/desktop/image-speaker-zx9.png">
-                  <img src="@/assets/imgs/home/mobile/image-speaker-zx9.png" alt="zx9_speaker" class="w-[172.25px] lg:w-[350px] pxl:w-[375px]">
+                  <img src="@/assets/imgs/home/mobile/image-speaker-zx9.png" alt="zx9_speaker" class="w-[172.25px] md:w-[197.21px]  lg:w-[350px] pxl:w-[375px]">
                </picture>
             </div>
 
@@ -152,6 +152,37 @@
          </div>
 
       </div>
+   </section>
+
+   <!-- ABOUT SECTION -->
+   <section class="about py-[60px] md:py-[48px] lg:py-[100px]">
+
+      <!-- MARGIN -->
+      <div class="margin flex flex-col items-center gap-10 md:gap-[63px] lg:flex-row-reverse">
+         <!-- IMAGE -->
+         <div class="overflow-hidden rounded-lg w-full lg:w-[50%]">
+            <picture>
+               <source media="(min-width: 1024px)" srcset="@/assets/imgs/shared/desktop/image-best-gear.jpg">
+               <source media="(min-width: 768px)" srcset="@/assets/imgs/shared/tablet/image-best-gear.jpg">
+               <img src="@/assets/imgs/shared/mobile/image-best-gear.jpg" alt="audiophile_image">
+            </picture>
+         </div>
+
+         <!-- TEXT -->
+         <div class="text-center max-w-[573px] w-full lg:text-left lg:pr-[20px] lg:w-[50%] lg:max-w-unset pxl:pr-[105px]">
+            <h2 class="heading-2 mb-8 max-w-[455px] mx-auto md:max-w-[unset]">
+               BRINGING YOU THE <span class="text-ad-brown">BEST</span> AUDIO GEAR
+            </h2>
+            
+            <p class="body-text">
+               Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, 
+               and audio accessories. We have a large showroom and luxury demonstration 
+               rooms available for you to browse and experience a wide range of our products. 
+               Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
+            </p>
+         </div>
+      </div>
+
    </section>
 
 </template>
